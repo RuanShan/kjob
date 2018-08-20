5d160ca117eafe237d4fcb0b2a0cc2ea116e8c5a
STM32King <30915620+STM32King@users.noreply.github.com>
2018-08-20 16:25:44 +0800

    招工列表--基本完成

--- src/pages/getworks/index.vue
+++ src/pages/getworks/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- 筛选 ===> START -->
     <div class="sizer">
       <div class="position-select">
         <picker class="" mode="region" :value="region" @change="CityChange">
@@ -7,11 +8,13 @@
         </picker>
       </div>
       <div class="work-select">
-        <button  @click="showPicker">工种筛选</button>
+        <button @click="showPicker">工种筛选</button>
         <mpvue-picker ref="mpvuePicker" :mode="mode" :deepLength="deepLength" :pickerValueArray="mulLinkageTwoPicker" :pickerValueDefault='pickerValueDefault' @onConfirm="onConfirm"></mpvue-picker>
       </div>
     </div>
+    <!-- 筛选 ===> END -->
 
+    <!-- 统计 ===> START -->
     <div class="statistics">
       <div class="works-num">
         今日在线招工数:
@@ -23,7 +26,64 @@
         <div></div>
       </div>
     </div>
-    
+    <!-- 统计 ===> END -->
+
+    <!-- 列表概览 ===> START -->
+    <div>
+      <div class="circular" v-for="item in jobList" :key="item" @click="detail">
+        <div class="top-----half">
+          <div class="one---row">
+            <div class="one-row-one">
+              {{item.city}}&nbsp;&nbsp;招&nbsp;-&nbsp;{{item.typeOfWork}}
+            </div>
+            <div class="one-row-two">
+              <div class="typeOfWork">
+                &nbsp;{{item.typeOfWork}}&nbsp;
+              </div>
+            </div>
+          </div>
+          <div class="two---row">
+            <div class="two-row-one">
+              价格:&nbsp;&nbsp;&nbsp;&nbsp;
+              <div class="price">
+                &nbsp;&nbsp;{{item.price}}
+              </div>
+            </div>
+            <div class="two-row-two">
+              工程量:
+              <div class="worksAmount">
+                &nbsp;&nbsp;{{item.worksAmount}}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="bottom-----half">
+          <div class="three---row">
+            <div class="three-row-one">
+              <img style="width: 40rpx; height: 40rpx;" src="../../../resources/icon/location.png"> {{item.state}}&nbsp;&nbsp;-&nbsp;&nbsp;{{item.city}}
+            </div>
+            <div class="three-row-two">
+              <img style="width: 40rpx; height: 40rpx;" src="../../../resources/icon/dateTime.png"> {{item.releaseTime}}
+            </div>
+          </div>
+          <div class="four---row">
+            <div class="four-row-one">
+              <div class="image">
+                <img style="width: 60rpx; height: 60rpx;" src="../../../resources/headImage/姜亿万.png">
+              </div>
+              <div class="publishName">
+                &nbsp;&nbsp;联系人&nbsp;&nbsp;-&nbsp;&nbsp;{{item.publishName}}
+              </div>
+            </div>
+            <div class="four-row-two">
+              查看详情 >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- 列表概览 ===> END -->
+
   </div>
 </template>
 
@@ -323,9 +383,53 @@ export default {
           ]
         }
       ],
+
       // ************招工数量和浏览人数************
       onLineworkersNum: '1234', // 招工数量
-      browsedTimes: '5678' // 浏览人数
+      browsedTimes: '5678', // 浏览人数
+
+      // ************招工列表************
+      jobList: [{
+        city: '广西河池', // 城市
+        typeOfWork: '抹灰工', // 工种
+        price: '6.5', // 价格
+        worksAmount: '7万平方米', // 工程量
+        state: '广西壮族自治区', // 省
+        releaseTime: '2018-06-06 10:55', // 时间
+        headImage: '', // 头像
+        publishName: '姜亿万' // 联系人姓名
+      },
+      {
+        city: '广西河池', // 城市
+        typeOfWork: '抹灰工', // 工种
+        price: '6.5', // 价格
+        worksAmount: '7万平方米', // 工程量
+        state: '广西壮族自治区', // 省
+        releaseTime: '2018-06-06 10:55', // 时间
+        headImage: '', // 头像
+        publishName: '姜亿万' // 联系人姓名
+      },
+      {
+        city: '广西河池', // 城市
+        typeOfWork: '抹灰工', // 工种
+        price: '6.5', // 价格
+        worksAmount: '7万平方米', // 工程量
+        state: '广西壮族自治区', // 省
+        releaseTime: '2018-06-06 10:55', // 时间
+        headImage: '', // 头像
+        publishName: '姜亿万' // 联系人姓名
+      },
+      {
+        city: '广西河池', // 城市
+        typeOfWork: '抹灰工', // 工种
+        price: '6.5', // 价格
+        worksAmount: '7万平方米', // 工程量
+        state: '广西壮族自治区', // 省
+        releaseTime: '2018-06-06 10:55', // 时间
+        headImage: '', // 头像
+        publishName: '姜亿万' // 联系人姓名
+      }
+      ] // 招工列表数据
     }
   },
 
@@ -358,7 +462,7 @@ export default {
       this.$refs.mpvuePicker.show()
     },
     onConfirm (e) {
-      console.log(e)
+      // console.log (e)
       let tempArray = e
       this.mulLinkageTwoPicker.forEach((elem) => {
         if (tempArray[0] === elem.value) {
@@ -371,6 +475,10 @@ export default {
         }
       })
       console.log(this.resultTypeOfWork)
+    },
+    // 点击展开详情
+    detail () {
+      console.log('Detail !!!')
     }
   }
 }
@@ -380,42 +488,155 @@ export default {
 .sizer {
   display: flex;
   flex-direction: row;
-    .position-select {
-      width: 50%;
-    }
-    .work-select {
-      width: 50%;
-    }
+  .position-select {
+    width: 50%;
+  }
+  .work-select {
+    width: 50%;
+  }
 }
 
 .statistics {
   height: 36rpx;
-  border:1px solid #c0c0c0;
+  border: 1px solid #c0c0c0;
   display: flex;
   flex-direction: row;
-    .works-num {
-      display: flex;
-      flex-direction: row;
-      justify-content: center;
-      width: 50%;
-      font-size:25rpx;
-      text-align:center;
-      .works-num-color {
-        color:red;
-      }
+  .works-num {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    width: 50%;
+    font-size: 25rpx;
+    text-align: center;
+    .works-num-color {
+      color: red;
     }
-    .people-num {
-      display: flex;
-      flex-direction: row;
-      justify-content: center;
-      width: 50%;
-      font-size:25rpx;
-      text-align:center;
-      .people-num-color {
-        color:red;
-      }
+  }
+  .people-num {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    width: 50%;
+    font-size: 25rpx;
+    text-align: center;
+    .people-num-color {
+      color: red;
     }
+  }
 }
-
-
+.circular {
+  margin: 25rpx auto;
+  background: #ffffff;
+  width: 700rpx;
+  height: 300rpx;
+  border: 2px solid #d6d6d6;
+  border-radius: 30rpx;
+  // line-height: 200px;
+  display: flex;
+  flex-direction: column;
+  .top-----half {
+    // background-color: cadetblue;
+    height: 50%;
+    display: flex;
+    flex-direction: column;
+    .one---row {
+      height: 60%;
+      // background-color:snow;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content:space-between;
+      .one-row-one {
+        // width: 70%;
+        padding-left: 25rpx;
+        font-size: 50rpx;
+        font-weight: bold;
+      }
+      .one-row-two {
+        // margin-right: 25rxp;
+        padding-right: 25rpx;
+        // border-radius: 30rpx;
+        // width: 30%;
+        // background-color: blueviolet;
+        .typeOfWork {
+          font-size: 35rpx;
+          border-radius: 30rpx;
+          background-color: #97cbff;
+           border: 1px solid #808080;
+        }
+      }
+    }
+    .two---row {
+      height: 40%;
+      // background-color:chartreuse;
+      display: flex;
+      flex-direction: row;
+      justify-content:space-between;
+      align-items: center;
+       .two-row-one {
+        padding-left: 25rpx;
+        display: flex;
+        .price {
+          color: #fc0a77;
+        }
+      }
+      .two-row-two {
+        // width: 60%;
+        // background-color:cornsilk;
+        padding-right: 25rpx;
+        display: flex;
+        .worksAmount {
+          color: #fc0a77;
+        }
+      }
+    }
+  }
+  .bottom-----half {
+    // background-color: salmon;
+    height: 50%;
+    display: flex;
+    flex-direction: column;
+    .three---row {
+      height: 40%;
+      // background-color:snow;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content:space-between;
+      font-size: 25rpx;
+      color: #808080;
+      .three-row-one {
+        padding-left: 25rpx;
+        // width: 50%;
+      }
+      .three-row-two {
+        padding-right: 25rpx;
+        // width: 50%;
+        // background-color: blueviolet;
+      }
+    }
+    .four---row {
+      height: 60%;
+      padding-left: 25rpx;
+      padding-right: 25rpx;
+      // background-color:chartreuse;
+      border-top: solid 1rpx #b8b8b8;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content:space-between;
+       .four-row-one {
+        display: flex;
+        align-items: center;
+        // width: 70%;
+      }
+      .four-row-two {
+        font-size: 25rpx;
+        color: #c0c0c0;
+        // width: 30%;
+        // background-color:cornsilk;
+      }
+    }
+  }
+}
 </style>
--- src/pages/test/index.vue
+++ src/pages/test/index.vue
@@ -1,147 +1,238 @@
 <template>
-  <div class="content">
-    <div class="circular">
+  <div>
+    <div class="circular" v-for="item in jobList" :key="item" @click="detail">
       <div class="top-----half">
         <div class="one---row">
           <div class="one-row-one">
-            <!-- {{city}} -->
+            {{item.city}}&nbsp;&nbsp;招&nbsp;-&nbsp;{{item.typeOfWork}}
           </div>
           <div class="one-row-two">
-            <!-- {{typeOfWork}} -->
+            <div class="typeOfWork">
+              &nbsp;{{item.typeOfWork}}&nbsp;
+            </div>
           </div>
         </div>
         <div class="two---row">
           <div class="two-row-one">
-
+            价格:&nbsp;&nbsp;&nbsp;&nbsp;
+            <div class="price">
+              &nbsp;&nbsp;{{item.price}}
+            </div>
           </div>
           <div class="two-row-two">
-            
+            工程量:
+            <div class="worksAmount">
+              &nbsp;&nbsp;{{item.worksAmount}}
+            </div>
           </div>
         </div>
       </div>
       <div class="bottom-----half">
         <div class="three---row">
-          <div class="three-row-one weui-form-preview__labe">
-            <!-- {{city}} -->
-            adlfjasdf
+          <div class="three-row-one">
+            <img style="width: 40rpx; height: 40rpx;" src="../../../resources/icon/location.png">
+            {{item.state}}&nbsp;&nbsp;-&nbsp;&nbsp;{{item.city}}
           </div>
           <div class="three-row-two">
-            {{typeOfWork}}
+            <img style="width: 40rpx; height: 40rpx;" src="../../../resources/icon/dateTime.png">
+            {{item.releaseTime}}
           </div>
         </div>
         <div class="four---row">
           <div class="four-row-one">
-
+            <div class="image">
+              <img style="width: 60rpx; height: 60rpx;" src="../../../resources/headImage/姜亿万.png">
+            </div>
+              <div class="publishName">
+                &nbsp;&nbsp;联系人&nbsp;&nbsp;-&nbsp;&nbsp;{{item.publishName}}
+              </div>
           </div>
           <div class="four-row-two">
-            
+            查看详情 >
           </div>
         </div>
       </div>
     </div>
   </div>
+    
 </template>
 
 <script>
-import base64 from '../../images/base64'
 export default {
   data () {
     return {
-      icon20: base64.icon20,
-      icon60: base64.icon60,
-      city: '广西河池', // 城市
-      typeOfWork: '抹灰工', // 工种
-      price: '6.5', // 价格
-      worksAmount: '7万平方米', // 工程量
-      state: '广西壮族自治区', // 省
-      releaseTime: '2018-06-06 10:55', // 时间
-      headImage: '', // 头像
-      publishName: '姜亿万' // 联系人姓名
+      jobList: [{
+        city: '广西河池', // 城市
+        typeOfWork: '抹灰工', // 工种
+        price: '6.5', // 价格
+        worksAmount: '7万平方米', // 工程量
+        state: '广西壮族自治区', // 省
+        releaseTime: '2018-06-06 10:55', // 时间
+        headImage: '', // 头像
+        publishName: '姜亿万' // 联系人姓名
+      },
+      {
+        city: '广西河池', // 城市
+        typeOfWork: '抹灰工', // 工种
+        price: '6.5', // 价格
+        worksAmount: '7万平方米', // 工程量
+        state: '广西壮族自治区', // 省
+        releaseTime: '2018-06-06 10:55', // 时间
+        headImage: '', // 头像
+        publishName: '姜亿万' // 联系人姓名
+      },
+      {
+        city: '广西河池', // 城市
+        typeOfWork: '抹灰工', // 工种
+        price: '6.5', // 价格
+        worksAmount: '7万平方米', // 工程量
+        state: '广西壮族自治区', // 省
+        releaseTime: '2018-06-06 10:55', // 时间
+        headImage: '', // 头像
+        publishName: '姜亿万' // 联系人姓名
+      },
+      {
+        city: '广西河池', // 城市
+        typeOfWork: '抹灰工', // 工种
+        price: '6.5', // 价格
+        worksAmount: '7万平方米', // 工程量
+        state: '广西壮族自治区', // 省
+        releaseTime: '2018-06-06 10:55', // 时间
+        headImage: '', // 头像
+        publishName: '姜亿万' // 联系人姓名
+      }
+      ] // 招工列表数据
     }
   },
   async onLoad () {
+    wx.setBackgroundColor({
+      backgroundColor: '#dadada'
+    })
+  },
+  methods: {
+    // 点击展开详情
+    detail () {
+      console.log('Detail !!!')
+    }
   }
 }
 </script>
 
 <style scoped lang='scss'>
-page {
-  background-color: #e2e2e2;
-}
-.content {
-  
-}
+// page {
+//   background-color: #dadada;
+// }
 .circular {
   margin: 25rpx auto;
   background: #ffffff;
   width: 700rpx;
   height: 350rpx;
   border: 2px solid #d6d6d6;
-  border-radius: 30px;
-  line-height: 200px;
+  border-radius: 30rpx;
+  // line-height: 200px;
   display: flex;
   flex-direction: column;
   .top-----half {
-    background-color: cadetblue;
+    // background-color: cadetblue;
     height: 50%;
     display: flex;
     flex-direction: column;
     .one---row {
       height: 60%;
-      background-color:snow;
+      // background-color:snow;
       display: flex;
       flex-direction: row;
+      align-items: center;
+      justify-content:space-between;
       .one-row-one {
-        width: 70%;
+        // width: 70%;
+        padding-left: 25rpx;
+        font-size: 50rpx;
+        font-weight: bold;
       }
       .one-row-two {
-        width: 30%;
-        background-color: blueviolet;
+        // margin-right: 25rxp;
+        padding-right: 25rpx;
+        // border-radius: 30rpx;
+        // width: 30%;
+        // background-color: blueviolet;
+        .typeOfWork {
+          font-size: 35rpx;
+          border-radius: 30rpx;
+          background-color: #97cbff;
+           border: 1px solid #808080;
+        }
       }
     }
     .two---row {
       height: 40%;
-      background-color:chartreuse;
+      // background-color:chartreuse;
       display: flex;
       flex-direction: row;
+      justify-content:space-between;
+      align-items: center;
        .two-row-one {
-        width: 40%;
+        padding-left: 25rpx;
+        display: flex;
+        .price {
+          color: #fc0a77;
+        }
       }
       .two-row-two {
-        width: 60%;
-        background-color:cornsilk;
+        // width: 60%;
+        // background-color:cornsilk;
+        padding-right: 25rpx;
+        display: flex;
+        .worksAmount {
+          color: #fc0a77;
+        }
       }
     }
   }
   .bottom-----half {
-    background-color: salmon;
+    // background-color: salmon;
     height: 50%;
     display: flex;
     flex-direction: column;
     .three---row {
       height: 40%;
-      background-color:snow;
+      // background-color:snow;
       display: flex;
       flex-direction: row;
+      align-items: center;
+      justify-content:space-between;
+      font-size: 25rpx;
+      color: #808080;
       .three-row-one {
-        width: 50%;
+        padding-left: 25rpx;
+        // width: 50%;
       }
       .three-row-two {
-        width: 50%;
-        background-color: blueviolet;
+        padding-right: 25rpx;
+        // width: 50%;
+        // background-color: blueviolet;
       }
     }
     .four---row {
       height: 60%;
-      background-color:chartreuse;
+      padding-left: 25rpx;
+      padding-right: 25rpx;
+      // background-color:chartreuse;
+      border-top: solid 1rpx #b8b8b8;
       display: flex;
       flex-direction: row;
+      align-items: center;
+      justify-content:space-between;
        .four-row-one {
-        width: 70%;
+        display: flex;
+        align-items: center;
+        // width: 70%;
       }
       .four-row-two {
-        width: 30%;
-        background-color:cornsilk;
+        font-size: 25rpx;
+        color: #c0c0c0;
+        // width: 30%;
+        // background-color:cornsilk;
       }
     }
   }
