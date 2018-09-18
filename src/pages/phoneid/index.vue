089c1ba776eab81676ff993c34ab9113bde4fc07
STM32King <30915620+STM32King@users.noreply.github.com>
2018-09-18 16:50:32 +0800

    60S倒序,重发

--- src/pages/phoneid/index.vue
+++ src/pages/phoneid/index.vue
@@ -60,7 +60,7 @@ export default {
       phoneNum: null, // 用户输入的手机号码
       getCode: '免费获取验证码', // 1.显示字样; 2.点击按钮后计时,并显示时间
       getCodeButtonDis: false, // 免费获取验证吗按钮是否禁用开关
-      timeNum: 0, // 时间计时
+      timeNum: 60, // 时间计时
       verifyCode: '', // 用户输入的收到的验证码
       inputVerifyCode: '', // 用户输入的校验码
       cheItem: { name: '阅读并同意以下条款', value: 1, checked: false }, // checkBox的数据
@@ -96,7 +96,7 @@ export default {
     freeGetverifyCode () {
       // 手机号码规则
       let phoneReg = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1})|(17[0-9]{1}))+\d{8})$/
-      let returnTime = null
+      let returnTimer = null
       console.log('点击了按钮')
       if (this.phoneNum !== null) { // 不为空,用户填写了数字
         if (phoneReg.test(this.phoneNum)) { // 根据规则校验
@@ -114,15 +114,15 @@ export default {
 
           // 禁用,计时,然后变字样
           this.getCodeButtonDis = true // 免费获取验证码按钮禁用
-          returnTime = setInterval(() => {
-            this.timeNum++
+          returnTimer = setInterval(() => {
+            this.timeNum--
             this.getCode = "" + this.timeNum + "秒后重发"
             console.log(this.getCode);
-            if (this.timeNum === 60) {
+            if (this.timeNum === 0) {
               this.getCodeButtonDis = false // 免费获取验证码按钮打开
               this.getCode = '免费获取验证码'
-              this.timeNum = 0
-              clearInterval(returnTime)
+              this.timeNum = 60
+              clearInterval(returnTimer)
             }
           }, 1000)
         } else { // 非法号码
